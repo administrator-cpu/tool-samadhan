d884481d8815e3eae7eb40da692404918cb34d4c
Ajay Negi <ajaynegi3345@icloud.com>
2026-08-04 13:55:37 +0530

    feat: add crmApiUrl to environment configuration

--- apps/backend/src/config/environment.ts
+++ apps/backend/src/config/environment.ts
@@ -70,6 +70,7 @@ export const env = {
     sender: getEnvStr('SMS_SENDER', ''),
   },
 
+  crmApiUrl: getEnvStr('CRM_API_URL', ''),
   crmNewApiUrl: getEnvStr('CRM_NEW_API_URL', ''),
   crmApiKey: getEnvStr('CRM_API_KEY', ''),
 
